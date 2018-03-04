8fa4764b39ac0802242c1d29d73b1105af7f03a0
Artem Starostenko <artemstarostenko65@gmail.com>
2018-03-03 21:12:35 -0800

    Add builtin functions test

---
+++ test.tf
@@ -0,0 +1,29 @@
+provider local {}
+
+variable "my_list" {
+  default = ["hello", "world"]
+}
+
+variable "my_map" {
+  default = {
+    id      = "1"
+    message = "hello world"
+  }
+}
+
+## functions to test
+output "my_list_test1" {
+  value = "${length(var.my_list)}"
+}
+
+output "my_map_test1" {
+  value = "${lookup(var.my_map, "id")}"
+}
+
+output "my_map_test2" {
+  value = "${lookup(var.my_map, "message")}"
+}
+
+output "my_map_test3" {
+  value = "${lookup(var.my_map, "author", "None")}"
+}
